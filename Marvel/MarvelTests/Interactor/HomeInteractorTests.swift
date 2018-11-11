0443fe8303fd7dc97412b705726ca9994c35127c
NSViking <victorvicente19@gmail.com>
2018-11-11 21:24:42 +0100

    Added missing Interactor tests
    Start with Presenter tests

---
+++ Marvel/MarvelTests/Interactor/HomeInteractorTests.swift
@@ -0,0 +1,71 @@
+//
+//  HomeInteractorTests.swift
+//  MarvelTests
+//
+//  Created by Víctor Vicente on 11/11/2018.
+//  Copyright © 2018 DevSouls. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Moya
+import RxSwift
+import RxBlocking
+import Mockit
+
+@testable import Marvel
+
+class HomeInteractorTests: XCTestCase {
+	
+	var mockRepo: ComicsRepositoryMock!
+	var pagination: Pagination!
+	var interactor: HomeInteractor!
+	
+	override func setUp() {
+		self.mockRepo = ComicsRepositoryMock(testCase: self)
+		self.pagination = Pagination()
+		
+		self.interactor = HomeInteractor(repo: self.mockRepo,
+										 pagination: self.pagination)
+	}
+
+	func testGetComics() {
+		
+		let mockResponse: [Comic] = []
+		let _ = mockRepo.when()
+			.call(withReturnValue: mockRepo.getComics(pagination: pagination))
+			.thenReturn(Single.just(mockResponse))
+		
+		do {
+			let results = try self.interactor.getComics()
+				.toBlocking()
+				.single()
+			XCTAssertTrue(results.count == 0)
+			XCTAssertTrue(self.interactor.pagination.getCurrentPage() == 0)
+		} catch {
+			XCTFail()
+		}
+		
+		let _ = mockRepo.verify(verificationMode: Once()).getComics(pagination: pagination)
+	}
+	
+	func testGetMoreUsers() {
+		
+		let mockResponse: [Comic] = []
+		let _ = mockRepo.when()
+			.call(withReturnValue: mockRepo.getComics(pagination: pagination))
+			.thenReturn(Single.just(mockResponse))
+		
+		do {
+			let results = try self.interactor.getMoreComics()
+				.toBlocking()
+				.single()
+			XCTAssertTrue(results.count == 0)
+			XCTAssertTrue(self.interactor.pagination.getCurrentPage() == 5)
+		} catch {
+			XCTFail()
+		}
+		
+		let _ = mockRepo.verify(verificationMode: Once()).getComics(pagination: pagination)
+	}
+}
---
+++ Marvel/MarvelTests/Presenter/HomePresenterTests.swift
@@ -0,0 +1,10 @@
+//
+//  HomePresenterTests.swift
+//  MarvelTests
+//
+//  Created by Víctor Vicente on 11/11/2018.
+//  Copyright © 2018 DevSouls. All rights reserved.
+//
+
+import Foundation
+
